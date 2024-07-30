31dbe8927ebb802421a9108ba47f2b33e6cf40e2
Pedro Daniel Jardim <pedrodanieljardim@hotmail.com>
2024-07-29 22:21:42 -0300

    feat: DiagonalDiference

--- Results/DiagonalDiference.java
+++ Results/DiagonalDiference.java
@@ -8,34 +8,71 @@ import static java.util.stream.Collectors.toList;
 public class DiagonalDiference {
     public static int diagonalDifference(List<List<Integer>> arr) {
         // Write your code here
-        return 0;
+
+        int n = arr.size();
+        int primaryDiagonal = 0;
+        int secondaryDiagonal = 0;
+
+        for(int i = 0; i < n; i++) {
+            List<Integer> row = arr.get(i);
+            int m = row.size();
+
+            for(int j = 0; j < m; j++){
+                if(i == j){
+                    System.out.println("to primaryDiagonal: " + row.get(j) + " at i: " + i + " j: " + j);
+                    primaryDiagonal += row.get(j);
+                }
+
+                if(i + j == n - 1){
+                    System.out.println("to secondaryDiagonal: " + row.get(j) + " at i: " + i + " j: " + j);
+                    secondaryDiagonal += row.get(j);
+                }
+
+            }
+
+
+        }
+
+        System.out.println("primaryDiagonal: " + primaryDiagonal);
+        System.out.println("secondaryDiagonal: " + secondaryDiagonal);
+
+        return Math.abs(primaryDiagonal - secondaryDiagonal);
+
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        long timeBefore = System.currentTimeMillis();
 
         List<List<Integer>> arr = new ArrayList<>();
 
-        IntStream.range(0, n).forEach(i -> {
-            try {
-                arr.add(
-                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                                .map(Integer::parseInt)
-                                .collect(toList()));
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        });
+        List<Integer> row1 = new ArrayList<>();
+        row1.add(11);
+        row1.add(2);
+        row1.add(4);
+
+        List<Integer> row2 = new ArrayList<>();
+        row2.add(4);
+        row2.add(5);
+        row2.add(6);
+
+        List<Integer> row3 = new ArrayList<>();
+        row3.add(10);
+        row3.add(8);
+        row3.add(-12);
+
+        arr.add(row1);
+        arr.add(row2);
+        arr.add(row3);
+
+
 
         int result = diagonalDifference(arr);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
+        System.out.println("Result: " + result);
 
-        bufferedReader.close();
-        bufferedWriter.close();
+        long timeAfter = System.currentTimeMillis();
+        long elapsed = timeAfter - timeBefore;
+
+        System.out.println("elapsed: " + elapsed + " ms");
     }
 }
